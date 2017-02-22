d546ce515368b4532e88ef4b9bdd6e7c2f37bc96
Owen Smith <owen@omsmith.ca>
2017-02-22 16:57:43 -0500

    fuzz: use native-crypto as keygen for fuzzing

--- fuzz/happy.js
+++ fuzz/happy.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var assert = require('assert'),
-	crypto = require('crypto');
-
-var keygen = require('./keygen');
+	crypto = require('crypto'),
+	jwkToPem = require('jwk-to-pem'),
+	nCrypto = require('native-crypto');
 
 var conv = require('../');
 
@@ -13,17 +13,28 @@ var sslalgs = {
 	'ES512': 'RSA-SHA512'
 };
 
+var crvs = {
+	ES256: 'P-256',
+	ES384: 'P-384',
+	ES512: 'P-521'
+};
+
 ['ES256', 'ES384', 'ES512'].forEach(function(alg) {
 	for (var i = 0; i < 10; ++i) {
-		var pem = keygen(alg);
+		nCrypto
+			.generate(crvs[alg])
+			.then(function(keypair) {
+				return jwkToPem(keypair.privateKey, { private: true });
+			})
+			.then(function(pem) {
+				for (var j = 0; j < 10000; ++j) {
+					var der = crypto
+						.createSign(sslalgs[alg])
+						.update(crypto.randomBytes((Math.random() * (256 - 1) | 0) + 1))
+						.sign(pem);
 
-		for (var j = 0; j < 10000; ++j) {
-			var der = crypto
-				.createSign(sslalgs[alg])
-				.update(crypto.randomBytes((Math.random() * (256 - 1) | 0) + 1))
-				.sign(pem);
-
-			assert(conv.joseToDer(conv.derToJose(der, alg), alg).equals(der));
-		}
+					assert(conv.joseToDer(conv.derToJose(der, alg), alg).equals(der));
+				}
+			});
 	}
 });
